620b7b1046a882edee75d5ba9dc05e9f2ad89488
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-03-11 22:09:25 +0300

    adds main.js, index.html and array.js

---
+++ development/code/arrays/arrays.js
@@ -0,0 +1,31 @@
+const shoppinglist = [
+    'barsoap',
+    'vaseline',
+    'milk',
+    'rice',
+    'quoarter',
+    'milk',
+];
+
+const students = [
+    {
+name:'',
+gender:'',
+role:'',
+    },
+    {
+        name:'',
+        gender:'',
+        role:'',
+            },
+            {
+                name:'',
+                gender:'',
+                role:'',
+                    },
+                    {
+                        name:'',
+                        gender:'',
+                        role:'',
+                            },
+]
